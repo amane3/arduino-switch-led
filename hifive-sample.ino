feabc34b21a47930b86edb2e003cbbd590ea602c
amane3 <38863546+amane3@users.noreply.github.com>
2019-01-10 00:47:46 +0530

    Update hifive-sample.ino

--- hifive-sample.ino
+++ hifive-sample.ino
@@ -5,6 +5,18 @@ Input Serial for changing led functions.
 // the setup function runs once when you press reset or power the board
 void* __dso_handle;
 
+const int READ_PINS = 1;
+const int WRITE_ANALOG = 2;
+const int WRITE_DIGITAL = 3;
+
+const int START_MSG = 0;
+const int END_MSG = 0;
+
+int incommingByte=0; 
+int outputByte;
+int i;
+int inputMsg;
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   analogWrite(LED_BUILTIN, 255);
@@ -13,15 +25,25 @@ void setup() {
 
 
 void loop(){
-int inputchar; 
-inputchar = Serial.read();
- 
-  if(inputchar != -1 ){
-   switch(inputchar){
-      int i;
-      case 'b':
-        Serial.print("LED BLINK\n");
-        for(i=0;i<256;i++){
+
+if(Serial.available() > 0){
+    // input available byte
+    incommingByte = Serial.read();
+
+      if(incommingByte == '1'){
+        
+        inputMsg=START_MSG;
+        Serial.write('0');
+        
+      }else if(incommingByte == '2'){
+        
+        outputByte = Serial.read();
+        if(outputByte == '0'){
+          analogWrite(LED_BUILTIN, 255);
+        }else if(outputByte == '1'){
+          analogWrite(LED_BUILTIN, 0);
+        }else if(outputByte == '2'){
+          for(i=0;i<256;i++){
           analogWrite(LED_BUILTIN, i);
           delay(3);
           }        
@@ -29,17 +51,10 @@ inputchar = Serial.read();
           analogWrite(LED_BUILTIN, i);
           delay(3);
           }
-        break;
-      case 'f':  
-        Serial.print("LED OFF\n");
-        analogWrite(LED_BUILTIN, 0);
-        break;
-      case 'o':
-        Serial.print("LED ON\n");
-        analogWrite(LED_BUILTIN, 255);
-        break;
-    }
-  } else { 
-    
-  }
+        }
+      }else{
+      }
+        
+   
+}
 }
